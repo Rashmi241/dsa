3459fc4e0bcbabda3d7c2e9c90fbe58f3c9b3b52
Rashmi241 <98954270+Rashmi241@users.noreply.github.com>
2022-03-08 15:32:16 +0530

    Add files via upload

---
+++ pr4.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+int n;
+void selectionsort(int a[])
+{
+int i,j,temp,pos;
+for(i=0;i<n-i;i++)
+{
+pos=i;
+for(j=j+1;j<n;j++)
+{
+if(a[pos]<a[j])
+{
+pos =j;
+}
+}
+if(pos!=i)
+{
+temp=a[i];
+a[i]=a[pos];
+a[pos]=temp;
+}
+}
+}
+int main()
+{
+int a[30],i;
+printf("enter the number of element:\n");
+scanf("%d",&n);
+printf("enter the array element\n");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+selectionsort(a);
+printf("the element after selection sort:");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+return 0;
+}
