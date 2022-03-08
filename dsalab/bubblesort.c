38e49a69d2e334e7f4bde1f5caf784979778a16c
Rashmi241 <98954270+Rashmi241@users.noreply.github.com>
2022-03-08 07:42:27 +0530

    Add files via upload

---
+++ dsalab/bubblesort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int n;
+void bubblesort(int a[])
+{
+int i,j,temp;
+for(i=0;i<n-1;i++)
+{
+for(j=0;j<n-i-1;j++)
+{
+if (a[i]>a[j+1])
+{
+temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+}
+}
+}
+}
+int main()
+{
+int a[10],i;
+printf("enter the number of element:\n");
+scanf("%d",&n);
+printf("enter the array element\n");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+bubblesort(a);
+printf(" sorted order:");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
+return 0;
+}
