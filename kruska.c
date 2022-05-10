6f9fbd6cbd8b01a94d7b3af3b287f6d3226d4020
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2022-05-10 22:15:50 +0530

    edited

--- kruska.c
+++ kruska.c
@@ -1,31 +1,39 @@
 #include<stdio.h>
-int i,j,n,cost[30][30],v1,v2;
-int i,edgwt,mincost=0;
+int i,j,n,cost[15][15],v1=0,v2=0;
 void doUnion(int root[],int v1,int v2){
-	int temp;
+	int temp,i;
 	temp=root[v2];
-	for(i=0;i<n;i++){
+	for(i=0;i<n;i++)
+	{
 		if(root[i]==temp)
+		{
 			root[i]=root[v1];
+	}
 	}
 }
 void findMin(){
-	int min;
+	int edgewt=99,i,j;
 	for(i=0;i<n;i++)
-		for(j=0;j<n;j++){
-			if(cost[i][j]<min && cost[i][j]>0){
-				min=cost[i][j];
+	{
+		for(j=0;j<n;j++)
+		{
+			if(cost[i][j]>0 && cost[i][j]<edgewt)
+			   {
+				edgewt=cost[i][j];
 				v1=i;
 				v2=j;		
 		}	
 	}
-
+			   }
 }
-void kruskal(int n){
+void kruskal()
+{
+	int,edgewt;
 	int root[n];
+	
 	for(i=0;i<n;i++)
 		root[i]=i;
-	printf("Edges of minimum spanning tree are ");
+	printf("Edges of minimum cost spanning tree are ");
 	i=0;
 	while(i!=n-1){
 		findMin();
@@ -34,7 +42,7 @@ void kruskal(int n){
 		if(root[v1]!=root[v2]){
 			printf("\n(%d,%d)",v1,v2);
 			doUnion(root,v1,v2);
-			mincost+=edgwt;
+			mincost+=edgewt;
 			i++;
 		}
 	}
@@ -45,16 +53,22 @@ void main()
        int i,j;
 	printf("Enter the number of vertices");
 	scanf("%d",&n);
-	printf("\nEnter the weights for the graph");
+	printf("\nEnter the undirected graph as adjecency matrix\n");
 	
+	
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			scanf("%d",&cost[i][j]);
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
 		{
-			scanf("%d",&cost[i][j]);
-			}
-			printf("\n");
-}	kruskal(n);
+			printf("%d",cost[i][j]);
+		}
+		printf("/n");
+	}
+			
+kruskal();
 }
 
 
