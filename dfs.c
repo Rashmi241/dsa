60a7ad8006a697f93c11ec7ca83b532ab7fa9164
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2024-07-01 15:06:40 +0530

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -2,13 +2,13 @@
 int a[6][6],n,visited[10],count=0;
 void dfs(int v)
 {
-int w;
-count ++;
-visited[v]=count;
-for(w=0;w<n;w++)
-if(visited[w]==0 && a[v][w]==1)
-dfs(w);
-printf("\n%d\t",v);
+   int w;  
+   count ++;
+   visited[v]=count;
+   for(w=0;w<n;w++)
+   if(visited[w]==0 && a[v][w]==1)
+      dfs(w);
+   printf("\n%d\t",v);
 }
 void main()
 {
@@ -21,16 +21,16 @@ for(j=0;j<n;j++)
 scanf("%d",&a[i][j]);
 printf("adjecency matrix:\n");
 for(i=0;i<n;i++)
-{
-for(j=0;j<n;j++)
-printf("%d\t",a[i][j]);
-printf("\n");
-}
+  {
+   for(j=0;j<n;j++)
+       printf("%d\t",a[i][j]);
+       printf("\n");
+  }
 for(i=0;i<n;i++)
-visited[i]==0;
-dfs(0);
+ visited[i]==0;
+ dfs(0);
 if(count==n)
-printf("\nthe graph is connected\n");
+   printf("\nthe graph is connected\n");
 else
-printf("\ngraph is not connected\n");
+   printf("\ngraph is not connected\n");
 }
